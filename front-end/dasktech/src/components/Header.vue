07cb8bcc209f85e68f3ca9d36503488b81ae2cb8
DanielTruscaV1 <danieltrusca2008@gmail.com>
2024-03-27 13:15:13 +0200

    Created and linked the header component.

---
+++ front-end/dasktech/src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+    @media screen and (max-width:600px)
+    {
+
+    }
+    @media screen and (min-width:600px)
+    {
+        
+    }
+</style>
