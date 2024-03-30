20f5ba2ae9ce02b5ca5dfb310464bde9dca7d8d8
DanielTruscaV1 <danieltrusca2008@gmail.com>
2024-03-30 22:08:46 +0200

    Updated the subject component.

--- front-end/dasktech/src/components/Subject1.vue
+++ front-end/dasktech/src/components/Subject1.vue
@@ -3,16 +3,71 @@
 </script>
 
 <template>
-  Subject
+  <div id="container">
+    <h1 id="title">
+      Artificial Intelligence
+    </h1>
+    <div id="list">
+      <div class="article">
+        What is Artificial Intelligence?
+      </div>
+      <div class="article">
+        Types of AI
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+    #container 
+    {
+      width:100vw;
+      height:80vh;
+      background-color:var(--color1);
+      font-family:var(--font1);
+    }
+    #title 
+    {
+      width:100vw;
+      position:absolute;
+      top:calc(10vh + 70px);
+      text-align:center;
+    }
+    #list
+    {
+      position:absolute;
+      top:calc(10vh + 150px);
+      width:100vw;
+      height:70vh;
+    }
+    .article 
+    {
+      width:100vw;
+      margin-top:30px;
+      padding-top:20px;
+      padding-bottom:20px;
+      text-align:center;
+      background-color:var(--color2);
+      color:var(--color1);
+      border-left:10px solid var(--color4);
+    }
     @media screen and (max-width:600px)
     {
 
     }
     @media screen and (min-width:600px)
     {
-        
+      .article 
+      {
+        width:50vw;
+        margin-top:30px;
+        margin-left:calc(25vw - 50px);
+        padding-top:20px;
+        padding-bottom:20px;
+        text-align:center;
+        background-color:var(--color2);
+        color:var(--color1);
+        border-left:50px solid var(--color4);
+      }
     }
 </style>
